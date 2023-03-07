3b33f1963c27046cb4af2e37ba90322a297e1a42
JonatanCespedes <retratos.jonatan@gmail.com>
2023-03-07 20:40:25 -0300

    Agrega middleware de rutas admin

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -11,20 +11,20 @@ const {
 } = require("../controllers/adminController");
 const uploadProductFile = require("../middlewares/uploadProductFile")
 const productValidator = require("../validations/productValidator");
-
+const sessionAdminCheck = require("../middlewares/sessionAdminCheck");
 /* GET - Index */
-router.get("/", index);
+router.get("/", sessionAdminCheck, index);
 
 /* PRODUCTS */
 
 /* GET - Products list */
-router.get("/products", products);
+router.get("/products", sessionAdminCheck, products);
 /* GET - Product create form */
-router.get("/products/create", create);
+router.get("/products/create", sessionAdminCheck, create);
 /* POST - Product store */
 router.post("/products/create", uploadProductFile.array("images"), productValidator, store);
 /* GET - Product edit form */
-router.get("/products/edit/:id", edit);
+router.get("/products/edit/:id", sessionAdminCheck, edit);
 /* PUT - Product update */
 router.put("/products/edit/:id", uploadProductFile.array("images"), productValidator, update);
 /* DELETE - Product delete */
